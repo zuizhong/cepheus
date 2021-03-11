f38cbcf566e2aefc822c9bdd9cc8fed91ed04de0
Can Guo <cang@codeaurora.org>
2021-03-10 22:56:17 -0800

    scsi: ufs-qcom: Turn off PHY only if link is not active
    Do not assume that AH8 always puts the link to hibern8 state before
    turnning off PHY during suspend. Turn off PHY only if link is not
    active.
    
    Change-Id: Ia50fdbe95e29e825b40679ba4519b49e2806b67a
    Signed-off-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1636,13 +1636,8 @@ static int ufs_qcom_setup_clocks(struct ufs_hba *hba, bool on,
 		err = ufs_qcom_ice_suspend(host);
 		if (err)
 			goto out;
-
-		/*
-		 * If auto hibern8 is enabled then the link will already
-		 * be in hibern8 state and the ref clock can be gated.
-		 */
-		if ((ufshcd_is_auto_hibern8_enabled(hba) ||
-		    !ufs_qcom_is_link_active(hba))) {
+							
+		if (!ufs_qcom_is_link_active(hba)) {
 			/* disable device ref_clk */
 			ufs_qcom_dev_ref_clk_ctrl(host, false);
 
