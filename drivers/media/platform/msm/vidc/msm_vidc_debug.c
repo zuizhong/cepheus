a01ab41bd2a57c30d45db106d5201c232c833fe7
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-24 04:46:09 +0530

    vidc: stub out msm_vidc_debug
    we're running without debugfs and this spams a lot so pls stahp

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -31,6 +31,7 @@ bool msm_vidc_thermal_mitigation_disabled = !true;
 int msm_vidc_clock_voting = !1;
 bool msm_vidc_syscache_disable = !true;
 
+#if 0
 #define MAX_DBG_BUF_SIZE 4096
 
 #define DYNAMIC_BUF_OWNER(__binfo) ({ \
@@ -545,3 +546,4 @@ int msm_vidc_check_ratelimit(void)
 	return __ratelimit(&_rs);
 }
 
+#endif
